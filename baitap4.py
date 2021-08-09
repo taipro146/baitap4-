c7596680a53e5fb1a9c4e0fd54e9f1bef8dafeba
taipro146 <88499355+taipro146@users.noreply.github.com>
2021-08-09 20:57:12 +0700

    Add files via upload

---
+++ baitap4.py
@@ -0,0 +1,43 @@
+# SquareSpiral3.py
+import turtle
+t = turtle.Pen()
+t.pencolor("blue")
+# vẽ hình vuông
+t.forward(50)
+t.left(90)
+for i in range(3):
+    t.forward(100)
+    t.left(90)
+t.forward(50)
+t.left(72)
+t.forward(50)
+t.left(90)
+for i in range(3):
+    t.forward(100)
+    t.left(90)
+t.forward(50)
+t.left(72)
+t.forward(50)
+t.left(90)
+for i in range(3):
+    t.forward(100)
+    t.left(90)
+t.forward(50)
+t.left(72)
+t.forward(50)
+t.left(90)
+for i in range(3):
+    t.forward(100)
+    t.left(90)
+t.forward(50)
+t.left(72)
+t.forward(50)
+t.left(90)
+for i in range(3):
+    t.forward(100)
+    t.left(90)
+t.forward(50)
+t.left(72)
+
+
+
